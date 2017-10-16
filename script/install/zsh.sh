3921c0ac43d2e2f083a4d9e062f7e8b0b4a15d01
nyo <billy31210@gmail.com>
2017-10-16 22:10:01 +0800

    add install zsh script

---
+++ script/install/zsh.sh
@@ -0,0 +1,8 @@
+
+# zsh
+
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# switch default shell to zsh
+chsh -s /bin/zsh
