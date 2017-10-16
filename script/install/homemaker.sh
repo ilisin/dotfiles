98c99bd28f0212398139c7db941ef1a178dbc9fb
nyo <billy31210@gmail.com>
2017-10-16 22:05:19 +0800

    add install homemaker script

---
+++ script/install/homemaker.sh
@@ -0,0 +1,9 @@
+
+
+# homemaker
+
+curl -O https://foosoft.net/projects/homemaker/dl/homemaker_linux_amd64.tar.gz
+tar zxvf homemaker_linux_amd64.tar.gz
+cp homemaker_linux_amd64/homemaker $HOME/bin/
+rm -r homemaker_linux_amd64
+rm homemaker_linux_amd64.tar.gz
