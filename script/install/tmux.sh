c6b240f3d88d1f016766a1f3c37a0f7fb3d16380
nyo <billy31210@gmail.com>
2017-11-21 13:42:36 +0800

    add tmux install script

---
+++ script/install/tmux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION=2.6
+sudo apt -y remove tmux
+sudo apt -y install wget tar libevent-dev libncurses-dev
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+tar xf tmux-${VERSION}.tar.gz
+rm -f tmux-${VERSION}.tar.gz
+cd tmux-${VERSION}
+./configure
+make
+sudo make install
+cd -
+sudo rm -rf /usr/local/src/tmux-*
+sudo mv tmux-${VERSION} /usr/local/src
+
