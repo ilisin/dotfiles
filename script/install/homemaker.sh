0594d280a27d5de5e0d3b292b2999896a45a8111
nyo <billy31210@gmail.com>
2017-11-21 13:40:56 +0800

    fix empty dir

--- script/install/homemaker.sh
+++ script/install/homemaker.sh
@@ -4,6 +4,7 @@
 
 curl -O https://foosoft.net/projects/homemaker/dl/homemaker_linux_amd64.tar.gz
 tar zxvf homemaker_linux_amd64.tar.gz
+mkdir -p $HOME/bin/
 cp homemaker_linux_amd64/homemaker $HOME/bin/
 rm -r homemaker_linux_amd64
 rm homemaker_linux_amd64.tar.gz
