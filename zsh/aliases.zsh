6c00982b94e8fe19d53d6e7e677cf503fec62f8f
nyo <billy31210@gmail.com>
2018-06-26 17:11:41 +0800

    add parrot alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,3 +18,5 @@ alias gitpurge="git checkout master | git branch -r --merged | grep -v master | 
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 alias zshconfig='vim .zshrc'
+
+alias parrot='curl parrot.live'
