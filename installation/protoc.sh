600d54b2db02430cfb1cc4f49800382f4444775b
nyo <billy31210@gmail.com>
2018-07-27 17:11:33 +0800

    update install protoc script

--- installation/protoc.sh
+++ installation/protoc.sh
@@ -10,9 +10,6 @@ FILENAME="protoc-$VERSION-$PLATFORM.zip"
 
 curl -LO "https://github.com/google/protobuf/releases/download/v$VERSION/$FILENAME"
 
-unzip $FILENAME -d /tmp/protoc-temp
-
-sudo cp /tmp/protoc-temp/bin/protoc /usr/local/bin
+sudo unzip -u $FILENAME -d /usr/local bin/protoc include/*
 
 rm $FILENAME
-rm -r /tmp/protoc-temp
