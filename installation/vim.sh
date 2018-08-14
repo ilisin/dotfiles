c9e310feb4470503dfb7d1c268a3ec8487a34774
nyo <billy31210@gmail.com>
2018-08-14 12:08:45 +0800

    add more installation

--- installation/vim.sh
+++ installation/vim.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # vim
 
-sudo apt install vim-nox ctags
+sudo apt install vim-nox vim-gtk ctags
 
-#curl http://j.mp/spf13-vim3 -L -o - | sh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
