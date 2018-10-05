c472a02fef9c7f8e0f5d20413f525b4a41f4b9a7
nyo <nyo4rdace@gmail.com>
2018-10-05 12:15:51 +0800

    add bat

--- installation/improve.sh
+++ installation/improve.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#sudo apt install bat
-
 mkdir -p ~/bin
 
+# diff-so-fancy
 wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
 chmod +x diff-so-fancy
 mv diff-so-fancy ~/bin
 
+# bat
+wget https://github.com/sharkdp/bat/releases/download/v0.7.1/bat_0.7.1_amd64.deb
+sudo dpkg -i bat_0.7.1_amd64.deb
+rm bat_0.7.1_amd64.deb
