ab144e1ed3e61cf0418feeada20dcd3cdba8762e
nyo <nyo4rdace@gmail.com>
2018-10-05 12:02:07 +0800

    add diff so fancy

---
+++ installation/improve.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#sudo apt install bat
+
+mkdir -p ~/bin
+
+wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
+chmod +x diff-so-fancy
+mv diff-so-fancy ~/bin
+
