fbd307efcf6a8dedbd70ebaf781977b2ce0f96f4
nyo <nyo4rdace@gmail.com>
2018-12-30 15:40:11 +0800

    update vim install script and vimrc

--- installation/vim.sh
+++ installation/vim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # vim
 
-sudo apt install vim-nox vim-gnome
+sudo apt install vim-nox vim-gtk3 vim-doc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
