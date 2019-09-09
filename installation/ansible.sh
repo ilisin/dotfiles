16c62057938f0dd0b50f7600e7d06786fd36988a
nyo <nyo4rdace@gmail.com>
2019-09-09 13:16:19 +0800

    add ansible install script

---
+++ installation/ansible.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' | sudo tee /etc/apt/sources.list.d/ansible.list
+
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+#sudo apt update
+#sudo apt install ansible
+
+sudo pip3 install ansible
