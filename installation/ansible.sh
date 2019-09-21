c57ddbac469a998fc9e736ae840c4aad45256958
nyo <nyo4rdace@gmail.com>
2019-09-21 20:06:01 +0800

    move installation scirpts

--- installation/ansible.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' | sudo tee /etc/apt/sources.list.d/ansible.list
-
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-#sudo apt update
-#sudo apt install ansible
-
-sudo pip3 install ansible
