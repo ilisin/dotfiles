27d02837a1186612cb36eb100ccfc150082e2e87
nyo <nyo4rdace@gmail.com>
2019-10-07 22:52:59 +0800

    fix cpu, mem status bar at mac

--- scripts/mac/tmux.sh
+++ scripts/mac/tmux.sh
@@ -6,3 +6,6 @@ brew install tmux
 # tmux exits with [exited] on mac os x
 # https://superuser.com/questions/397076/tmux-exits-with-exited-on-mac-os-x
 brew install reattach-to-user-namespace
+
+# CPU, RAM, and load monitor for use with tmux
+brew install tmux-mem-cpu-load
